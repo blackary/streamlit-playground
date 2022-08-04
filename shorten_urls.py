883526e59f62ac83e17431f83a8ece7872b44770
Zachary Blackwood <zachary@streamlit.io>
2022-08-04 11:20:59 -0400

    Create streamlit app with sharable url

---
+++ shorten_urls.py
@@ -0,0 +1,68 @@
+import hashlib
+import json
+from typing import Any, Dict
+from urllib import parse
+
+import pandas as pd
+import streamlit as st
+
+from db import insert_row, select_where
+
+TABLE = "url_table"
+DEFAULT_HASH_LENGTH = 6
+BASE_URL = "https://playground.streamlitapp.com"
+
+
+def get_hash(data: str, length: int = DEFAULT_HASH_LENGTH) -> str:
+    """
+    Given a string representation of data, return a length-characters-long string hash
+    """
+    return hashlib.md5(data.encode()).hexdigest()[:length]
+
+
+def get_python() -> str:
+    return st.session_state["python"]
+
+
+def get_hash_from_python() -> str:
+    return get_hash(get_python())
+
+
+def get_python_from_hash(hash: str) -> str:
+    row = select_where(TABLE, "python", "hash", hash)
+    return row[0]["python"]
+
+
+def is_hash_in_table(hash: str) -> bool:
+    row = select_where(TABLE, "hash", "hash", hash)
+    return len(row) > 0
+
+
+def save_hash_if_not_exists() -> str:
+    hash = get_hash_from_python()
+    if not is_hash_in_table(hash):
+        python = get_python()
+        insert_row({"hash": hash, "python": python}, TABLE)
+    return hash
+
+
+def get_short_url_from_hash(hash: str) -> str:
+    return BASE_URL + "?" + parse.urlencode({"q": hash})
+
+
+def get_short_url_button():
+    if st.button("Get shareable url"):
+        hash = save_hash_if_not_exists()
+        url = get_short_url_from_hash(hash)
+        st.write(url)
+
+
+def expand_short_url():
+    query_params = st.experimental_get_query_params()
+    if set(query_params.keys()) == {"q"}:
+        short_hash = query_params["q"][0]
+        try:
+            python = get_python_from_hash(short_hash)
+            st.session_state["python"] = python
+        except IndexError:
+            st.error(f"Invalid short url: {short_hash}")
---
+++ streamlit_app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from streamlit_ace import st_ace
+
+from shorten_urls import expand_short_url, get_short_url_button
+
+expand_short_url()
+
+if "python" not in st.session_state:
+    st.session_state["python"] = ""
+
+
+def execute(code: str):
+    try:
+        exec(code)
+    except Exception as e:
+        st.exception(e)
+
+
+if st.experimental_user["email"] in st.secrets["authorized_users"]:
+    python = st_ace(
+        value=st.session_state["python"],
+        key="python",
+        language="python",
+        min_lines=20,
+    )
+    get_short_url_button()
+
+else:
+    st.expander("Show code").code(st.session_state["python"])
+
+
+execute(st.session_state["python"])
